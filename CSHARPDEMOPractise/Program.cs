36b7465d51132e5ce1d905ecb7b369d92cbbb258
Blagoi Ivanov <hristovkaloyan11@gmail.com>
2024-06-04 17:10:30 +0300

    Making queries

--- CSHARPDEMOPractise/Program.cs
+++ CSHARPDEMOPractise/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
@@ -35,28 +36,125 @@ namespace CSHARPDEMOPractise
                     }
                 }
                 Console.WriteLine("--------------------------------------------------------");
-                var query2 = "SELECT v.Name as VillainName,STRING_AGG(m.Name, ','),STRING_AGG(m.Age, ',')  FROM Villains v JOIN MinionsVillains mv ON v.Id = mv.VillainId JOIN Minions m ON m.Id = mv.MinionId GROUP BY v.Name";
-                using(var cmd = new SqlCommand(query2, connection))
+                //GetMinionNamesByBillain(connection);
+                AddMinion(connection);
+
+
+                
+            }
+
+        }
+        private static void AddMinion(SqlConnection connection)
+        {
+            string minionInfo = Console.ReadLine();
+            string[] minionInfoArr = minionInfo.Substring(minionInfo.IndexOf(':') + 2).Split(' ');
+            string villainInfo = Console.ReadLine();
+            villainInfo = villainInfo.Substring(villainInfo.IndexOf(':') + 2);
+
+            string nameExists = "SELECT Name FROM Villains WHERE Name = @Name";
+            string TownExists = "SELECT Name FROM Towns WHERE NAME = @Town";
+            using (var cmd = new SqlCommand(nameExists, connection))
+            {
+                cmd.Parameters.AddWithValue("@Name", villainInfo);
+                var name = (string)cmd.ExecuteScalar();
+                if (name == null)
                 {
-                    using (var reader = cmd.ExecuteReader())
+                    string query = "INSERT INTO Villains (Name,EvilnessFactorId) VALUES (@Name,4)";
+                    using(var cmd2 = new SqlCommand(query, connection))
                     {
-                        while (reader.Read())
-                        {
-                            Console.Write($"{reader[0]} =>");
-                            var secondCol = ((string)reader[1]).Split(',');
-                            var AgeCol = ((string)reader[2]).Split(',');
-                            for (int i = 0; i < secondCol.Length; ++i)
-                            {
-                                Console.Write($"{AgeCol[i]}-{secondCol[i]},");
-                            }
-                            Console.WriteLine();
-                        }
+                        cmd2.Parameters.AddWithValue("@Name",villainInfo);
+                        cmd2.ExecuteNonQuery();
+
                     }
                 }
+                
 
+            }
+            using (var cmd3 = new SqlCommand(TownExists, connection))
+            {
+                cmd3.Parameters.AddWithValue("@Town", minionInfoArr[2]);
+                var townName = (string)cmd3.ExecuteScalar();
+                if(townName == null)
+                {
+                    string query = "INSERT INTO Towns (Name,CountryCode) VALUES (@Name,2)";
+                    using( var cmd4 = new SqlCommand(query, connection))
+                    {
+                        cmd4.Parameters.AddWithValue("@Name", minionInfoArr[2]);
+                        cmd4.ExecuteNonQuery();
+                    }
+                }
+            }
+            var minionQuery = "INSERT INTO Minions (Name,Age,TownId) VALUES (@Name,@Age,@TownId)";
+            var townIDQuery = "SELECT Id FROM Towns WHERE Town = @Town";
+            using(var cmd5 =  new SqlCommand(minionQuery, connection))
+            {
+                using (var cmd6 = new SqlCommand(townIDQuery, connection))
+                {
+                    cmd6.Parameters.AddWithValue("@Town", minionInfoArr[2]);
+                    var id = (int)cmd5.ExecuteScalar();
+                    cmd5.Parameters.AddWithValue("@Name", minionInfoArr[0]);
+                    cmd5.Parameters.AddWithValue("@Age", minionInfoArr[1]);
+                    cmd5.Parameters.AddWithValue("@TownId", id);
+
+                }
+                
+            }
+            
+
+
+        }
+        private static void GetMinionNamesByBillain(SqlConnection connection)
+        {
+            int id = int.Parse(Console.ReadLine());
+            string villainQuery = "SELECT Name FROM Villains WHERE id = @Id";
+
+            using (var command = new SqlCommand(villainQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
+                var villainName = command.ExecuteScalar();
+                Console.WriteLine($"Villain-{villainName}");
+                string minionquery = "SELECT m.Name,Age FROM Minions m JOIN MinionsVillains mv ON m.Id = mv.MinionId JOIN Villains v ON mv.VillainId = v.Id WHERE v.Name = @Name";
+                using (var cmd = new SqlCommand(minionquery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Name", villainName);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        int count = 1;
+                        var read = reader.Read();
+                        if (!read)
+                        {
+                            Console.WriteLine("No minions");
+                        }
+                        while (read)
+                        {
+                            Console.WriteLine($"{count}:Name:{reader["Name"]}-Age:{reader["Age"]}");
+                            ++count;
+                            read = reader.Read();
+                        }
+
+                    }
+
+
+                }
 
             }
 
+        }
+        private static void GetVillainName(SqlConnection connection)
+        {
+            string query = "SELECT Name, COUNT(mv.MinionId) FROM Villains AS v JOIN MinionsVillains AS mv ON mv.VillainId = v.Id GROUP BY v.Id,v.Name";
+            using(var command = new SqlCommand(query, connection))
+            {
+                using(var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader[0];
+                        var count = reader[1];
+                        Console.WriteLine($"{name} - {count}");
+                    }
+                }
+            }
         }
         private static void ExecuteNonQuery(SqlConnection connection ,string query)
         {
